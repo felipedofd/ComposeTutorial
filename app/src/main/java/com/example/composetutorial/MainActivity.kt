c7b569d3a00bb6541d8ac81db52c07568276d46c
Felipe Anzolin <felipedofd@hotmail.com>
2023-06-26 17:56:45 -0300

    foi criado um componente de IU simples com funções declarativas para estudos do jetpack compose.
    1: funções de composição,
    2: layouts,
    3: Material Design,
    4: listas e animações.

---
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -0,0 +1,157 @@
+package com.example.composetutorial
+
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composetutorial.ui.theme.ComposeTutorialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeTutorialTheme {
+                Row {
+                    Conversation(
+                        listOf(
+                            Message(
+                                "TRAFICANTE",
+                                "COLOMBINHA NO 16 A GCOLOMBINHA NO 16 A GCOLOMBINHA NO 16 A GCOLOMBINHA NO 16 A GCOLOMBINHA NO 16 A GCOLOMBINHA NO 16 A GCOLOMBINHA NO 16 A GCOLOMBINHA NO 16 A GCOLOMBINHA NO 16 A GCOLOMBINHA NO 16 A GCOLOMBINHA NO 16 A GCOLOMBINHA NO 16 A GCOLOMBINHA NO 16 A GCOLOMBINHA NO 16 A GCOLOMBINHA NO 16 A GCOLOMBINHA NO 16 A GCOLOMBINHA NO 16 A GCOLOMBINHA NO 16 A GCOLOMBINHA NO 16 A GCOLOMBINHA NO 16 A GCOLOMBINHA NO 16 A G"
+                            )
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: com.example.composetutorial.Message) {
+    Row(
+        modifier = Modifier.padding(all = 8.dp)
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+
+        var isExpanded by remember { mutableStateOf(false) }
+        val surfaceColor by animateColorAsState(targetValue = if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface)
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author, color = MaterialTheme.colorScheme.secondary
+            )
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(
+                shape = MaterialTheme.shapes.small,
+                shadowElevation = 10.dp,
+                tonalElevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
+        }
+    }
+}
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true, name = "Dark Mode"
+)
+@Composable
+fun PreviewMessageCard() {
+    ComposeTutorialTheme {
+        Surface {
+            MessageCard(
+                msg = com.example.composetutorial.Message(
+                    "Colleague", "Hey, take a loot at jetpack Compose, it`s great!"
+                )
+            )
+        }
+    }
+}
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(msg = message)
+            MessageCard(msg = message)
+            MessageCard(msg = message)
+            MessageCard(msg = message)
+            MessageCard(msg = message)
+            MessageCard(msg = message)
+            MessageCard(msg = message)
+            MessageCard(msg = message)
+            MessageCard(msg = message)
+            MessageCard(msg = message)
+            MessageCard(msg = message)
+            MessageCard(msg = message)
+            MessageCard(msg = message)
+            MessageCard(msg = message)
+            MessageCard(msg = message)
+            MessageCard(msg = message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation() {
+    ComposeTutorialTheme {
+        Conversation(
+            listOf(
+                Message(
+                    "Colleague", "Hey, take a loot at jetpack Compose, it`s great!"
+                )
+            )
+        )
+    }
+}
